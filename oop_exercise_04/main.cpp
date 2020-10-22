cbeafb1e6cc07b2b3285bbd7311d706817d24537
Evgenii Pishchik <57502208+Pe4enIks@users.noreply.github.com>
2020-10-22 04:15:10 +0300

    oop_4

--- oop_exercise_04/main.cpp
+++ oop_exercise_04/main.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <cmath>
 #include <algorithm>
+#include <fstream>
 
 //Константа "pi"
 #define PI 3.14159265
@@ -100,36 +101,81 @@ double square_tuple(T& tuple)
 
 int main()
 {
-    //Создаем три экземпляра шаблонных классов
-    Square<int> sq;
-    Triangle<double> tr;
-    Octagon<double> ogon;
+    std::string name = "start";
+	std::cout << "Enter q or Q or quit or Quit for close programm" << std::endl;
+	//Бесконечный дикл до ввода ключевого слова для выхода
+	while(true)
+	{
+		std::cout << "Enter test file name: ";
+		std::cin >> name;
+		if (name == "Q" || name == "q" || name == "Quit" || name == "quit") break;
+		//поток ввода из файла, имя файла хранится в string переменной name
+		std::ifstream finp(name.c_str());
+        std::string s11, s12, s21, s22, s31, s32, s41, s42;
+        std::string t11, t12, t21, t22, t31, t32;
+        std::string p11, p12, p21, p22, p31, p32, p41, p42, p51, p52;
+        std::string p61, p62, p71, p72, p81, p82;
+        //Взятие 16 чисел для восьмиугольника из файла
+		finp >> p11 >> p12 >> p21 >> p22 >> p31 >> p32 >> p41 >> p42 >> p51 >> p52 >> p61 >> p62 >> p71 >> p72 >> p81 >> p82;
+        //Взятие 8 чисел для квадрата из файла
+        finp >> s11 >> s12 >> s21 >> s22 >> s31 >> s32 >> s41 >> s42;
+        //Взятие 6 чисел для треугольника из файла
+        finp >> t11 >> t12 >> t21 >> t22 >> t31 >> t32;
+        try
+        {
+            //Создаем три экземпляра шаблонных классов
+            Square<int> sq;
+            Triangle<double> tr;
+            Octagon<double> ogon;
 
-    //Инициализация
-    sq.v_arr[0] = {0,0};
-    sq.v_arr[1] = {1,1};
-    sq.v_arr[2] = {0,1};
-    sq.v_arr[3] = {1,0};
+            //Инициализация
+            sq.v_arr[0].first = std::stoi(s11);
+            sq.v_arr[0].second = std::stoi(s12);
+            sq.v_arr[1].first = std::stoi(s21);
+            sq.v_arr[1].second = std::stoi(s22);
+            sq.v_arr[2].first = std::stoi(s31);
+            sq.v_arr[2].second = std::stoi(s32);
+            sq.v_arr[3].first = std::stoi(s41);
+            sq.v_arr[3].second = std::stoi(s42);
 
-    tr.v_arr[0] = {1.0,0.0};
-    tr.v_arr[1] = {0.0,0.0};
-    tr.v_arr[2] = {0.5,sqrt(0.75)};
+            tr.v_arr[0].first = std::stod(t11);
+            tr.v_arr[0].second = std::stod(t12);
+            tr.v_arr[1].first = std::stod(t21);
+            tr.v_arr[1].second = std::stod(t22);
+            tr.v_arr[2].first = std::stod(t31);
+            tr.v_arr[2].second = std::stod(t32);
 
-    ogon.v_arr[0] = {sqrt(2.0),sqrt(2.0)+2.0};
-    ogon.v_arr[1] = {sqrt(2.0)+1.0,1.0};
-    ogon.v_arr[2] = {-1.0,1.0};
-    ogon.v_arr[3] = {0.0,sqrt(2.0)+2.0};
-    ogon.v_arr[4] = {0.0,0.0};
-    ogon.v_arr[5] = {sqrt(2.0),0.0};
-    ogon.v_arr[6] = {-1.0,1.0+sqrt(2.0)};
-    ogon.v_arr[7] = {sqrt(2.0)+1.0,sqrt(2.0)+1.0};
+            ogon.v_arr[0].first = std::stod(p11);
+            ogon.v_arr[0].second = std::stod(p12);
+            ogon.v_arr[1].first = std::stod(p21);
+            ogon.v_arr[1].second = std::stod(p22);
+            ogon.v_arr[2].first = std::stod(p31);
+            ogon.v_arr[2].second = std::stod(p32);
+            ogon.v_arr[3].first = std::stod(p41);
+            ogon.v_arr[3].second = std::stod(p42);
+            ogon.v_arr[4].first = std::stod(p51);
+            ogon.v_arr[4].second = std::stod(p52);
+            ogon.v_arr[5].first = std::stod(p61);
+            ogon.v_arr[5].second = std::stod(p62);
+            ogon.v_arr[6].first = std::stod(p71);
+            ogon.v_arr[6].second = std::stod(p72);
+            ogon.v_arr[7].first = std::stod(p81);
+            ogon.v_arr[7].second = std::stod(p82);
 
-    //Создаем tuple из фигур
-    auto tuple = std::make_tuple(tr, sq, ogon);
-    //Печать tuple фигур
-    print_tuple<decltype(tuple),0>(tuple);
-    //Печать суммарной площади фигур в tuple
-    std::cout << "Square: " << square_tuple<decltype(tuple),0>(tuple) << std::endl;
+            //Создаем tuple из фигур
+            auto tuple = std::make_tuple(tr, sq, ogon);
+            //Печать tuple фигур
+            print_tuple<decltype(tuple),0>(tuple);
+            //Печать суммарной площади фигур в tuple
+            std::cout << "Square: " << square_tuple<decltype(tuple),0>(tuple) << std::endl;
+        }
+        catch(std::exception& ex)
+        {
+            std::cerr << "Incorrect data" << std::endl;
+            std::cerr << ex.what() << std::endl;
+        }
+        finp.close();
+    }
 
     return 0;
 }
